f0779d5021672782854ad447139ca575540acbbd
sneha13498 <sneha.mahadik@mastek.com>
2016-02-26 18:12:03 +0530

    peercontacts

---
+++ app/src/main/java/com/benow/models/PeerContact.java
@@ -0,0 +1,127 @@
+package com.benow.models;
+
+
+public class PeerContact {
+
+    private Integer id;
+    private String contactName;
+    private String mobileNumber;
+    private String mmid;
+    private String accountNumber;
+    private String ifscCode;
+    private String contactType;
+    private String appUserId;
+
+    /**
+     * @return The id
+     */
+    public Integer getId() {
+        return id;
+    }
+
+    /**
+     * @param id The id
+     */
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    /**
+     * @return The contactName
+     */
+    public String getContactName() {
+        return contactName;
+    }
+
+    /**
+     * @param contactName The contactName
+     */
+    public void setContactName(String contactName) {
+        this.contactName = contactName;
+    }
+
+    /**
+     * @return The mobileNumber
+     */
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    /**
+     * @param mobileNumber The mobileNumber
+     */
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    /**
+     * @return The mmid
+     */
+    public String getMmid() {
+        return mmid;
+    }
+
+    /**
+     * @param mmid The mmid
+     */
+    public void setMmid(String mmid) {
+        this.mmid = mmid;
+    }
+
+    /**
+     * @return The accountNumber
+     */
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    /**
+     * @param accountNumber The accountNumber
+     */
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    /**
+     * @return The ifscCode
+     */
+    public String getIfscCode() {
+        return ifscCode;
+    }
+
+    /**
+     * @param ifscCode The ifscCode
+     */
+    public void setIfscCode(String ifscCode) {
+        this.ifscCode = ifscCode;
+    }
+
+    /**
+     * @return The contactType
+     */
+    public String getContactType() {
+        return contactType;
+    }
+
+    /**
+     * @param contactType The contactType
+     */
+    public void setContactType(String contactType) {
+        this.contactType = contactType;
+    }
+
+    /**
+     * @return The appUserId
+     */
+    public String getAppUserId() {
+        return appUserId;
+    }
+
+    /**
+     * @param appUserId The appUserId
+     */
+    public void setAppUserId(String appUserId) {
+        this.appUserId = appUserId;
+    }
+
+}
